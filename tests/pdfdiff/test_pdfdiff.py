c81ee261367b569d928437542d708cdccf68002a
manhdt <thitkhotau.no1@gmail.com>
2026-02-08 18:19:47 +0700

    Refactor test code

--- tests/pdfdiff/test_pdfdiff.py
+++ tests/pdfdiff/test_pdfdiff.py
@@ -1,59 +1,38 @@
 import subprocess
+from contextlib import ExitStack
+from typing import Any
+
+import pytest
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 from tests.data.pdfs.create_pdf import create_pdf, Font
 
 
-def test_pdfdiff_same_pages_number() -> None:
-    pdf1 = Path("10pages.pdf")
-    pdf2 = Path("20pages1.pdf")
+@pytest.mark.parametrize("n1, n2",[
+    [1, 1],
+    [2, 2],
+    [1, 2],
+    [2, 1],
+    [100, 1],
+    [1, 100],
+])
+def test_pdfdiff(n1, n2) -> None:
+    with ExitStack[Any]() as stack:
+        p1 = stack.enter_context(NamedTemporaryFile(suffix="1.pdf"))
+        p2 = stack.enter_context(NamedTemporaryFile(suffix="2.pdf"))
 
-    create_pdf(10, Font.TIMES_ROMAN, pdf1)
-    create_pdf(10, Font.TIMES_ROMAN, pdf2)
+        pdf1 = Path(p1.name)
+        pdf2 = Path(p2.name)
 
-    output_file = "output.pdf"
+        create_pdf(n1, Font.TIMES_ROMAN, pdf1)
+        create_pdf(n2, Font.TIMES_ROMAN, pdf2)
 
-    subprocess.run([
-        "poetry", "run", "pdfdiff",
-        "--pdf1", pdf1.as_posix(),
-        "--pdf2", pdf2.as_posix(),
-        "--output-file", output_file
-    ])
+        output_file = "output.pdf"
 
-
-def test_pdfdiff_others_pages_number() -> None:
-    pdf1 = Path("1pages.pdf")
-    pdf2 = Path("12pages1.pdf")
-
-    create_pdf(3, Font.TIMES_ROMAN, pdf1)
-    create_pdf(2, Font.TIMES_ROMAN, pdf2)
-
-    output_file = Path("output.pdf")
-    output_file.unlink(missing_ok=True)
-
-    subprocess.run([
-        "poetry", "run", "pdfdiff",
-        "--pdf1", pdf1.as_posix(),
-        "--pdf2", pdf2.as_posix(),
-        "--output-file", output_file.as_posix()
-    ])
-    subprocess.run(["open", "output.pdf"])
-
-
-def test_pdfdiff_others_pages_number() -> None:
-    pdf1 = Path("1pages.pdf")
-    pdf2 = Path("12pages1.pdf")
-
-    create_pdf(3, Font.TIMES_ROMAN, pdf1)
-    create_pdf(4, Font.TIMES_ROMAN, pdf2)
-
-    output_file = Path("output.pdf")
-    output_file.unlink(missing_ok=True)
-
-    subprocess.run([
-        "poetry", "run", "pdfdiff",
-        "--pdf1", pdf1.as_posix(),
-        "--pdf2", pdf2.as_posix(),
-        "--output-file", output_file.as_posix()
-    ])
-    subprocess.run(["open", "output.pdf"])
+        subprocess.run([
+            "poetry", "run", "pdfdiff",
+            "--pdf1", pdf1.as_posix(),
+            "--pdf2", pdf2.as_posix(),
+            "--output-file", output_file
+        ])
